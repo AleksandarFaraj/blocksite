8258b3b391dc9de11be3d6a917bff6226dbc722c
hektr <hektr992@gmail.com>
2017-10-30 16:00:22 +0300

    update

--- js/background.js
+++ js/background.js
@@ -1,30 +1,28 @@
 
-//function regExpEscape(string) {
-//    return string.replace(/[-[\]{}()*+!<=:?.\/\\^$|#\s,]/g, '\\$&');
-//}
-
 function removeWhiteSpace(pattern) {
+    
     return pattern.replace(/\s/g, '');
-}    
+}
 
 function matchAndClose(pattern, url, tabId) {
-        if((pattern.length > 4)&&(/ /.test(pattern)) == false) {
-            
-            var re = new RegExp(pattern, 'i');
-            if( re.test(url) ) {
-                chrome.tabs.remove(tabId);
-//                alert(url + " was blocked");
-            }
+    if((pattern.length > 4)&&(/ /.test(pattern)) == false) {
+
+        var re = new RegExp(pattern, 'i');
+        if( re.test(url) ) {
+            chrome.tabs.remove(tabId);
         }
+    }
 }
 
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 
     if(typeof(changeInfo.url) !== 'undefined') {
+        
         chrome.storage.sync.get(null, function(items) {
+
             if(typeof(items.pattern) !== 'undefined') {
+                
                 changeInfo.url = changeInfo.url.match(/(?:http(s)?:\/\/)?.+?\//);
-//              alert(changeInfo.url[0]);
                 matchAndClose(removeWhiteSpace(items.pattern), changeInfo.url[0], tabId);
             }
        });
--- js/options.js
+++ js/options.js
@@ -1,23 +1,25 @@
 // Saves options to chrome.storage
 
 function save_options() {
+    
     var user_pattern = document.getElementById('user_pattern').value;
     
-    chrome.storage.sync.set({
-        pattern:  user_pattern,
-    }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
+    chrome.storage.sync.set({ pattern:  user_pattern }, function() {
+        
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+            status.textContent = '';
+        }, 750);
+    });
 }
 
 
 function restore_options() {
+    
     chrome.storage.sync.get(null, function(items) {
+        
         if(typeof(items.pattern) !== 'undefined') {
           document.getElementById('user_pattern').value = items.pattern;
       }
