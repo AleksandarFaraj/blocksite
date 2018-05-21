bd23a650f850232f0b86ea596ce2c13ca3f876e7
Aleksandar Faraj <aleksandarfaraj@gmail.com>
2018-05-21 23:48:21 +0200

    reduce disk read but sacrifice UX

--- js/background.js
+++ js/background.js
@@ -1,30 +1,33 @@
 
 function matchAndClose(pattern, url, tabId) {
-
     var re = new RegExp(pattern, 'i');
     if (re.test(url)) {
         //            alert(re);
-        
         updateProperties = new Object();
         updateProperties.url = 'https://aleksandarfaraj.github.io/blocksite/';
+        chrome.tabs.update(tabId, updateProperties, function () { });
         chrome.tabs.update(tabId, updateProperties, function () {
             // Anything else you want to do after the tab has been updated.
         });
     }
 };
-
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-
-    if ((typeof (changeInfo.url) !== 'undefined') && (! /(chrome:\/\/)/.test(changeInfo.url))) {
-        //        alert(changeInfo.url);
-
-        chrome.storage.sync.get(null, function (items) {
-
-            if ((typeof (items.pattern) !== 'undefined') && (items.pattern !== null) && (items.disable == false)) {
-                //                alert(items.pattern);
-                changeInfo.url = changeInfo.url.match(/(?:http(s)?:\/\/)?.+?\//);
-                matchAndClose(items.pattern, changeInfo.url[0], tabId);
+function matchTabFactory(cb) {
+    chrome.storage.sync.get(null, function (items) {
+        var matchTab = function (tabId, changeInfo, tab) {
+            if ((typeof (changeInfo.url) !== 'undefined') && (! /(chrome:\/\/)/.test(changeInfo.url))) {
+                //        alert(changeInfo.url);
+                if ((typeof (items.pattern) !== 'undefined') && (items.pattern !== null) && (items.disable == false)) {
+                    //                alert(items.pattern);
+                    changeInfo.url = changeInfo.url.match(/(?:http(s)?:\/\/)?.+?\//);
+                    matchAndClose(items.pattern, changeInfo.url[0], tabId);
+                }
             }
-        });
-    }
+        }
+        cb(matchTab);
+    });
+}
+matchTabFactory(function (matchTab) {
+    chrome.tabs.onActivated.addListener(matchTab);
+    chrome.tabs.onHighlighted.addListener(matchTab);
+    chrome.tabs.onUpdated.addListener(matchTab);
 });
